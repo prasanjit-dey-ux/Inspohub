afa3d6a1a6d92539c5df7c810f71e1fd2ac0e0d0
prasnjit-dey-ux <prasanjitdey8523@gmail.com>
2025-11-04 17:25:09 +0530

    updated the page

---
+++ client/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    
+    <div className="border-x  border-neutral-200 h-screen max-w-5xl bg-neutral-50 mx-auto">
+
+    </div>
+  );
+}
