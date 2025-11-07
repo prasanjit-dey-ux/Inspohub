b752dee41c31caced32cac7602bb955a8c4cfdf3
prasnjit-dey-ux <prasanjitdey8523@gmail.com>
2025-11-07 22:20:19 +0530

    updated home page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -6,13 +6,13 @@ import { Card } from "@/component/ui/card";
 export default function Home() {
   return (
     
-    <div className=" h-screen max-w-7xl mx-auto ">
+    <div className="min-h-screen max-w-7xl mx-auto px-6">
         <Navbar />
         <Header />
-        <div className="grid grid-cols-3 mt-6 gap-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-4 pb-20">
           <div>
             <Card 
-            src="/images/img1.png"
+            src="/images/img3.jpg"
             title="Appinspo"
             tag="Inspiration"
             
@@ -20,35 +20,35 @@ export default function Home() {
           </div>
           <div>
             <Card 
-            src="/images/img1.png"
+            src="/images/img4.jpg"
             title="Appinspo"
             tag="Inspiration"
             />
           </div>
                     <div>
             <Card 
-            src="/images/img1.png"
+            src="/images/img5.jpg"
             title="Appinspo"
             tag="Inspiration"
             />
           </div>
                               <div>
             <Card 
-            src="/images/img1.png"
+            src="/images/img6.png"
             title="Appinspo"
             tag="Inspiration"
             />
           </div>
                                         <div>
             <Card 
-            src="/images/img1.png"
+            src="/images/img7.jpg"
             title="Appinspo"
             tag="Inspiration"
             />
           </div>
                                         <div>
             <Card 
-            src="/images/img1.png"
+            src="/images/img8.jpg"
             title="Appinspo"
             tag="Inspiration"
             />
@@ -61,9 +61,10 @@ export default function Home() {
 
 const Header = () => {
 return (
-    <div className="py-20 pb-10 mt-40">
-      <h1 className="text-6xl font-medium text-neutral-950 tracking-tight uppercase max-w-3xl">
-        Discover websites that <span className="text-neutral-400">inspire creativity.</span>
+    <div className="pt-64 pb-10">
+      <h1 className="text-6xl font-medium  text-neutral-950 tracking-tight uppercase max-w-3xl">
+        Discover websites that{" "}
+        <span className="text-neutral-400">Inspire creativity.</span>
       </h1>
       <p className="text-neutral-500 text-base max-w-xl mt-6">
         Discover the best developer portfolios, design ideas, and tools — all in one place. Get inspired and create your next big thing with confidence.
@@ -75,9 +76,10 @@ return (
 const Navbar =  () => {
   return (
     <>
-      <nav>
-          <div className=" h-18 flex justify-between items-center ">
-          <h1 className="font-medium text-xl text-neutral-950 ">INSPIRE</h1>
+      <nav className="py-6">
+          <div className=" max-w-7xl mx-auto flex justify-between items-center">
+             <h1 className="font-medium text-xl text-neutral-950 ">INSPIRE</h1>
+
           <div className="flex gap-6 text-neutral-500 text-base ">
             <p>Home</p>
             <p>Portfolio</p>
