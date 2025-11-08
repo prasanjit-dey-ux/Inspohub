22fe73001459fa1a7e7c2def3ca63cec7f93ece4
prasnjit-dey-ux <prasanjitdey8523@gmail.com>
2025-11-08 17:23:12 +0530

    added site data

---
+++ client/data/siteData.ts
@@ -0,0 +1,79 @@
+import { SiteItem } from "@/types/site";
+
+export const sitesData: SiteItem[] = [
+  {
+    id: "i1",
+    title: "Appinspo",
+    tag: "Inspiration",
+    url: "https://www.appinspo.com/",
+    creator: "jordiplz",
+    image: "/images/appinspo.png",
+  },
+    {
+    id: "p1",
+    title: "Abhinav Kale",
+    tag: "Portfolio",
+    url: "https://abhi.at/",
+    creator: "Abhinavstwt",
+    image: "/images/avi.png",
+  },
+  {
+    id: "i2",
+    title: "Craftwork",
+    tag: "Inspiration",
+    url: "https://craftwork.design/",
+    creator: "craftworkdesign",
+    image: "/images/craftwork_light.png",
+  },
+        {
+    id: "p5",
+    title: "Chánh Đại",
+    tag: "Portfolio",
+    url: "https://chanhdai.com/",
+    creator: "iamncdai",
+    image: "/images/chanh.png",
+  },
+      {
+    id: "p2",
+    title: "OPM",
+    tag: "Portfolio",
+    url: "https://www.mihircodes.in/",
+    creator: "mihirtwt",
+    image: "/images/opm.png",
+  },
+
+  {
+    id: "t1",
+    title: "Aceternity UI",
+    tag: "Tool",
+    url: "https://ui.aceternity.com/",
+    creator: "mannupaaji",
+    logo: "/logos/aceternity.png",
+  },
+
+    {
+    id: "p3",
+    title: "Guri",
+    tag: "Portfolio",
+    url: "https://gxuri.in/",
+    creator: "Gur__vi",
+    image: "/images/guri.png",
+  },
+  {
+    id: "p4",
+    title: "Emil Kowalski",
+    tag: "Portfolio",
+    url: "https://emilkowal.ski/",
+    creator: "emilkowalski_",
+    image: "/images/emil_kowalski.png",
+  },
+  {
+    id: "i3",
+    title: "SEESAW",
+    tag: "Portfolio",
+    url: "https://www.seesaw.website/",
+    creator: "seesawsite",
+    image: "/images/seesaw.png",
+  },
+
+];
