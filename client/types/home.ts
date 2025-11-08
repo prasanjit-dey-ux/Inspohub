acc70038e1571a6e4ebe6fe176ad04e685447824
prasnjit-dey-ux <prasanjitdey8523@gmail.com>
2025-11-08 17:23:02 +0530

    updated home data

---
+++ client/types/home.ts
@@ -0,0 +1,8 @@
+// import { InspirationItem } from "./inspiration";
+// import { PortfolioItem } from "./portfolio";
+// import { ToolData } from "./tools";
+
+// export type HomeFeedItem = 
+//     | ({ type: "inspiration" } & InspirationItem)
+//     | ( { type: "portfolio" } & PortfolioItem)
+//     | ( { type: "tool" } & ToolData)
