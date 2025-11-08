d2106d97b95146dc8fda67813c53cfa6fa6440c2
prasnjit-dey-ux <prasanjitdey8523@gmail.com>
2025-11-08 17:24:12 +0530

    updated header

---
+++ client/component/layout/header.tsx
@@ -0,0 +1,20 @@
+
+type HeaderProps = {
+    title: string
+    highlight: string
+    description: string
+}
+
+export default function Header({title, description, highlight}: HeaderProps){
+  return (
+    <div className="pt-64 pb-10">
+      <h1 className="text-6xl font-medium  text-neutral-950 tracking-tight uppercase max-w-3xl">
+        {title}{" "}
+        <span className="text-neutral-400">{highlight}</span>
+      </h1>
+      <p className="text-neutral-400 text-base max-w-xl mt-6">
+        {description}
+      </p>
+  </div>
+  )
+}
