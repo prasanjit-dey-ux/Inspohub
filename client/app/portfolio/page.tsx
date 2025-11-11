37b1014dba256ae04f153d3e1a08c579ba42cccf
prasnjit-dey-ux <prasanjitdey8523@gmail.com>
2025-11-11 23:01:59 +0530

    updated portfolio page

---
+++ client/app/portfolio/page.tsx
@@ -0,0 +1,26 @@
+
+import Header from "@/component/layout/header";
+import Card from "@/component/ui/card";
+import { sitesData } from "@/data/siteData";
+
+export default function PortfolioPage() {
+  const portfolioItems = sitesData.filter((item) => item.tag === "Portfolio");
+
+  return (
+    <div className="min-h-screen max-w-7xl mx-auto px-6">
+
+      <Header
+        title="Explore developer"
+        highlight="Portfolios."
+        description="Discover curated portfolios of developers building beautiful, functional, and creative websites."
+      />
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-4 pb-20">
+        {portfolioItems.map((item) => (
+          <Card key={item.id} {...item} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
