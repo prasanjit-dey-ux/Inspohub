7d3e2b48cf61c097ec122ee2893756bbeae26c9c
prasnjit-dey-ux <prasanjitdey8523@gmail.com>
2025-11-11 23:02:26 +0530

    added motion in home page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,19 +1,26 @@
 
+'use client'
+
 import Card  from "@/component/ui/card";
 import { sitesData } from "@/data/siteData";
 import CardTool from "@/component/ui/toolCard";
 import Header from "@/component/layout/header";
-import Navbar from "@/component/layout/navbar";
+import { motion } from "motion/react";
 
 export default function Home() {
   return (
     
-    <div className="min-h-screen max-w-7xl mx-auto px-6">
-        <Navbar />
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5, ease: "easeOut" }}
+    
+    className="min-h-screen max-w-7xl mx-auto px-6">
+
         <Header 
           title="Discover websites that"
-          highlight="Inspire creativity."
-          description="        Discover the best developer portfolios, design ideas, and tools — all in one place. Get inspired and create your next big thing with confidence."
+          highlight="Inspire creativity"
+          description="Discover the best developer portfolios, design ideas, and tools — all in one place. Get inspired and create your next big thing with confidence."
         />
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-4 pb-20">
         {sitesData.map((item) =>
@@ -25,7 +32,7 @@ export default function Home() {
         )}
         </div>
   
-    </div>
+    </motion.div>
   );
 }
 
