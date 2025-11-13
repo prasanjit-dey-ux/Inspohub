6dd38026693a0e296976bdf9a170e48a7b9e3543
prasnjit-dey-ux <prasanjitdey8523@gmail.com>
2025-11-13 23:54:06 +0530

    updated container

--- client/component/ui/container.tsx
+++ client/component/ui/container.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export const Container = ({children, className}: {children: React.ReactNode, className?: string}) => {
   return (
-    <div className={clsx('max-w-7xl mx-auto px-6 min-h-screen border-x border-slate-200 ',className)}>
+    <div className={clsx('max-w-7xl  mx-auto px-6 min-h-screen  ',className)}>
         {children}
     </div>
   )
