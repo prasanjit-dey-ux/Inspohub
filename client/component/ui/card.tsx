f30445c184b9f827129bb062eb967f14377449aa
prasnjit-dey-ux <prasanjitdey8523@gmail.com>
2025-11-14 21:33:54 +0530

    updated card

--- client/component/ui/card.tsx
+++ client/component/ui/card.tsx
@@ -24,13 +24,13 @@ export default function Card({image, title, className, tag, url, creator}: SiteI
           )}
 
         <div className=' absolute inset-0 bg-black/70 opacity-0 group-hover:opacity-100 transition flex items-center justify-center'>
-          <span className='text-black rounded-md px-2 py-1 text-sm font-medium flex justify-center  items-center bg-white'>Visit →</span>
+          <span className='rounded-md px-2 py-1 text-sm font-medium flex justify-center items-center bg-neutral-50 text-neutral-950'>Visit →</span>
         </div>
       </div>
     </Link>
 
 <div className="flex justify-between items-end px-4 py-3 bg-neutral-50">
-    <div>
+    <div className='flex flex-col items-start justify-center gap-1'>
       <h3 className="text-sm font-semibold">{title}</h3>
       <p className="text-zinc-500 text-sm font-medium">{tag}</p>
     </div>
@@ -39,7 +39,7 @@ export default function Card({image, title, className, tag, url, creator}: SiteI
       <Link 
       href={`http://x.com/${creator.replace("@", "")}`}
       target='blank'
-      className='text-zinc-500 text-sm font-medium hover:underline hover:text-neutral-900'
+      className='text-zinc-500 text-sm font-medium hover:underline hover:text-blue-700'
       >
         {creator}
       </Link>
