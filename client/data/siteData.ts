c2c7c1ee9d939aba618513b61a386c16a996508d
prasnjit-dey-ux <prasanjitdey8523@gmail.com>
2025-11-29 21:07:20 +0530

    updated site data

--- client/data/siteData.ts
+++ client/data/siteData.ts
@@ -6,15 +6,16 @@ export const sitesData: SiteItem[] = [
     title: "Appinspo",
     tag: "Inspiration",
     url: "https://www.appinspo.com/",
-    creator: "jordiplz",
+    creator: "https://x.com/jordiplz",
     image: "/images/appinspo.png",
   },
-          {
+
+  {
     id: "p1",
     title: "Chánh Đại",
     tag: "Portfolio",
     url: "https://chanhdai.com/",
-    creator: "iamncdai",
+    creator: "https://x.com/iamncdai",
     image: "/images/chanh.png",
   },
 
@@ -23,16 +24,16 @@ export const sitesData: SiteItem[] = [
     title: "Craftwork",
     tag: "Inspiration",
     url: "https://craftwork.design/",
-    creator: "craftworkdesign",
+    creator: "https://x.com/craftworkdesign",
     image: "/images/craftwork_light.png",
   },
 
-      {
+  {
     id: "p2",
     title: "OPM",
     tag: "Portfolio",
     url: "https://www.mihircodes.in/",
-    creator: "mihirtwt",
+    creator: "https://x.com/mihirtwt",
     image: "/images/opm.png",
   },
 
@@ -41,123 +42,133 @@ export const sitesData: SiteItem[] = [
     title: "Aceternity UI",
     tag: "Tool",
     url: "https://ui.aceternity.com/",
-    creator: "mannupaaji",
+    creator: "https://x.com/mannupaaji",
     logo: "/logos/aceternity.png",
   },
-      {
+
+  {
     id: "p5",
     title: "Abhinav Kale",
     tag: "Portfolio",
     url: "https://abhi.at/",
-    creator: "Abhinavstwt",
+    creator: "https://x.com/Abhinavstwt",
     image: "/images/avi.png",
   },
 
-    {
+  {
     id: "p3",
     title: "Guri",
     tag: "Portfolio",
     url: "https://gxuri.in/",
-    creator: "Gur__vi",
+    creator: "https://x.com/Gur__vi",
     image: "/images/guri.png",
   },
+
   {
     id: "p4",
     title: "Emil Kowalski",
     tag: "Portfolio",
     url: "https://emilkowal.ski/",
-    creator: "emilkowalski_",
+    creator: "https://x.com/emilkowalski_",
     image: "/images/emil_kowalski.png",
   },
+
   {
     id: "i3",
     title: "SEESAW",
     tag: "Inspiration",
     url: "https://www.seesaw.website/",
-    creator: "seesawsite",
+    creator: "https://x.com/seesawsite",
     image: "/images/seesaw.png",
   },
+
   {
-  id: "i4",
-  title: "Awwwards",
-  tag: "Inspiration",
-  url: "https://www.awwwards.com/",
-  creator: "Awwwards",
-  image: "/images/awwwards.png",
-},
-{
-  id: "i5",
-  title: "Siteinspire",
-  tag: "Inspiration",
-  url: "https://www.siteinspire.com/",
-  creator: "siteinspire",
-  image: "/images/siteinspire.png",
-},
-{
-  id: "i6",
-  title: "Minimal Gallery",
-  tag: "Inspiration",
-  url: "https://minimal.gallery/",
-  creator: "minimal_gallery",
-  image: "/images/minimal.png",
-},
-{
-  id: "i7",
-  title: "Godly Website",
-  tag: "Inspiration",
-  url: "https://godly.website/",
-  creator: "GodlyWebsite",
-  image: "/images/godly.png",
-},
-{
-  id: "i8",
-  title: "Landingfolio",
-  tag: "Inspiration",
-  url: "https://www.landingfolio.com/",
-  creator: "landingfolio",
-  image: "/images/landingfolio.png",
-},
-{
-  id: "i9",
-  title: "Lapa Ninja",
-  tag: "Inspiration",
-  url: "https://www.lapa.ninja/",
-  creator: "lapaninja",
-  image: "/images/lapaninja.png",
-},
-{
-  id: "i10",
-  title: "Mobbin",
-  tag: "Inspiration",
-  url: "https://mobbin.com/",
-  creator: "mobbin",
-  image: "/images/mobbin1.png"
-},
+    id: "i4",
+    title: "Awwwards",
+    tag: "Inspiration",
+    url: "https://www.awwwards.com/",
+    creator: "https://x.com/awwwards",
+    image: "/images/awwwards.png",
+  },
 
-{
-  id: "i11",
-  title: "Carl Beaverson",
-  tag: "Portfolio",
-  url: "https://carlbeaverson.com",
-  creator: "CarlBeaverson",
-  image: "/images/carlbeaverson.png",
-},
-{
-  id: "i12",
-  title: "Designspells",
-  tag: "Inspiration",
-  url: "https://www.designspells.com",
-  creator: "designspells_",
-  image: "/images/designspells.png",
-},
-{
-  id: "i13",
-  title: "Sapiens",
-  tag: "Inspiration",
-  url: "https://sapiens.website",
-  creator: "sapiens",
-  image: "/images/sapiens.png",
-},
+  {
+    id: "i5",
+    title: "Siteinspire",
+    tag: "Inspiration",
+    url: "https://www.siteinspire.com/",
+    creator: "https://x.com/siteinspire",
+    image: "/images/siteinspire.png",
+  },
 
+  {
+    id: "i6",
+    title: "Minimal Gallery",
+    tag: "Inspiration",
+    url: "https://minimal.gallery/",
+    creator: "https://x.com/minimal_gallery",
+    image: "/images/minimal.png",
+  },
 
+  {
+    id: "i7",
+    title: "Godly Website",
+    tag: "Inspiration",
+    url: "https://godly.website/",
+    creator: "https://x.com/GodlyWebsite",
+    image: "/images/godly.png",
+  },
+
+  {
+    id: "i8",
+    title: "Landingfolio",
+    tag: "Inspiration",
+    url: "https://www.landingfolio.com/",
+    creator: "https://x.com/landingfolio",
+    image: "/images/landingfolio.png",
+  },
+
+  {
+    id: "i9",
+    title: "Lapa Ninja",
+    tag: "Inspiration",
+    url: "https://www.lapa.ninja/",
+    creator: "https://x.com/lapaninja",
+    image: "/images/lapaninja.png",
+  },
+
+  {
+    id: "i10",
+    title: "Mobbin",
+    tag: "Inspiration",
+    url: "https://mobbin.com/",
+    creator: "https://x.com/mobbin",
+    image: "/images/mobbin1.png",
+  },
+
+  {
+    id: "i11",
+    title: "Carl Beaverson",
+    tag: "Inspiration",
+    url: "https://carlbeaverson.com",
+    creator: "https://x.com/CarlBeaverson",
+    image: "/images/carlbeaverson.png",
+  },
+
+  {
+    id: "i12",
+    title: "Designspells",
+    tag: "Inspiration",
+    url: "https://www.designspells.com",
+    creator: "https://x.com/designspells_",
+    image: "/images/designspells.png",
+  },
+
+  {
+    id: "i13",
+    title: "Sapiens",
+    tag: "Inspiration",
+    url: "https://sapiens.website",
+    creator: "https://github.com/junaidanjum",
+    image: "/images/sapiens.png",
+  },
 ];
