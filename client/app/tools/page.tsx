af27bf0f97f09694756d9b0757d97d04c9bd37d2
prasnjit-dey-ux <prasanjitdey8523@gmail.com>
2025-12-15 08:06:42 +0530

    updated: the Tool page ux writing

--- client/app/tools/page.tsx
+++ client/app/tools/page.tsx
@@ -1,7 +1,11 @@
-
+"use client";
 import Header from "@/component/layout/header";
+import { ActionBar } from "@/component/ui/actionBar";
 import Card from "@/component/ui/card";
+import { PaginatedGrid } from "@/component/ui/paginationGrid";
 import { sitesData } from "@/data/siteData";
+
+
 
 export default function InspirationPage() {
   const ToolItems = sitesData.filter((item) => item.tag === "Tool");
@@ -10,17 +14,18 @@ export default function InspirationPage() {
     <div className="min-h-screen max-w-7xl mx-auto px-6">
 
       <Header
-        title="Tools that actually"
-        highlight="make you faster"
-        description="A curated collection of websites that balance design and functionality.
-Each one chosen to help developers and designers think beyond the ordinary."
+        title="Tools That Acctually "
+        highlight="Save Time"
+        description="AI tools, design utilities, and resources that make your workflow faster. Handpicked for developers and designers who want to ship better work."
+      />
+      <ActionBar />
+      <PaginatedGrid 
+        data={ToolItems}
+        itemsPerPage={15}
+        renderItem={(item) => <Card key={item.id} {...item}/>}
       />
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-4 pb-20">
-        {ToolItems.map((item) => (
-          <Card key={item.id} {...item} />
-        ))}
-      </div>
+
     </div>
   );
 }
