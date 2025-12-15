671ab4413e5d9cd6d3706060e7557e6c9e764e47
prasnjit-dey-ux <prasanjitdey8523@gmail.com>
2025-12-15 08:07:03 +0530

    Updated the footer: Made it responsive.

--- client/component/layout/footer.tsx
+++ client/component/layout/footer.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="py-6 ">
-      <div className="max-w-7xl mx-auto px-6 flex justify-between items-start mt-2">
+    <footer className="pt-6">
+      <div className="max-w-7xl mx-auto px-6 flex flex-col justify-between items-start md:flex-row gap-10 md:gap-0">
         
 
         <div className="flex flex-col gap-10">
@@ -12,15 +12,16 @@ export default function Footer() {
               <div className="bg-blue-700 size-6 flex justify-end items-baseline-last">
                 <div className="rounded-full size-4 bg-white"></div>
               </div>
-              INSPIIRE</h2>
-            <p className="mt-2 text-neutral-500 max-w-xs">
-              Curated inspiration for developers who think visually.
+              INSPOSITE
+            </h2>
+            <p className="mt-2 text-neutral-500  max-w-8/12">
+              Curated inspiration for people who think visually.
             </p>
           </div>
 
 
-          <p className="font-serif text-xl text-blue-700">
-            crafted with care — Prasanjit Dey
+          <p className=" mt-2 font-serif text-xl text-blue-700">
+            Crafted with care — Prasanjit Dey
           </p>
         </div>
 
@@ -28,24 +29,26 @@ export default function Footer() {
 
 
 
-          <div className="mt-2 flex flex-col gap-2 text-sm">
-            <Link href="https://github.com/prasanjit_dey_ux/" className="hover:text-blue-700 text-neutral-500">
+          <div className="mt-1 flex flex-col  justify-between items-start gap-2 text-base">
+            <h1 className="font-semibold">Explore</h1>
+            <Link href="/about" className=" hover:text-blue-700 text-neutral-500">
+            About
+            </Link>
+
+            <Link href="https://x.com/Prasanjit_ui" target="_blank" className="hover:text-blue-700 text-neutral-500">
+              Twitter / X
+            </Link> 
+
+            <Link href="https://github.com/prasanjit-dey-ux/" target="_blank" className="hover:text-blue-700 text-neutral-500">
               GitHub
-            </Link>
-            <Link href="https://x.com/Prasanjit_ui.com" className="hover:text-blue-700 text-neutral-500">
-              Twitter
-            </Link>
-            <Link href="/sponsor" className="hover:text-blue-700 text-neutral-500">
-              Sponsor
-            </Link>
-  
+            </Link> 
           </div>
         </div>
 
       </div>
 
-      <p className="mt-10 text-center text-sm text-neutral-500">
-        © 2025 INSPIRE — All content belong to their original owner.
+      <p className="mt-10 border-t py-2 border-gray-200 text-center md:text-sm text-neutral-500 text-xs">
+        © 2025 INSPOSITE — All linked content belongs to respective creators.
       </p>
     </footer>
   );
